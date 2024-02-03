36cf587474355f16bc0bd9e64fbecd0e9e22e168
LevanPangani5 <levanfangani55@gmail.com>
2024-02-03 15:05:44 +0400

    adds HW 14

---
+++ IT step/Home Works/HW14.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace IT_step.Home_Works
+{
+    internal class HW14
+    {
+        static void Login()
+        {
+            string password = passwordValidation();
+            Console.Write("\n\n");
+
+            //mail validation
+            string email;       //at least one lowercase uppercase number special emil
+            Regex EmailRegex = new(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&]{8,}@([\w-]{2,}\.)+[\w-]{2,4}$");
+            do
+            {
+                Console.WriteLine("!! warning !!");
+
+                Console.WriteLine("Email should contain at least one lower one Upper one number and one special character \n" +
+                "should be at least 12 characters long");
+
+                Console.WriteLine("Enter Email: ");
+
+                email = Console.ReadLine()!;
+                if (!EmailRegex.IsMatch(email))
+                {
+                    Console.WriteLine("\nInvalid Email try again ... \n\n");
+                }
+                else
+                {
+                    break;
+                }
+            } while (true);
+
+            Console.WriteLine($"\npassword: {password} and email:{email} are valid");
+        }
+
+
+        //password generation & validation
+        static string passwordValidation()
+        {
+            StringBuilder password = new();
+            ConsoleKeyInfo? info;
+            Regex PasswordRegex = new(@"(^[a-zA-Z0-9.!_%+-/]).{7,}$");
+
+            do
+            {
+                Console.WriteLine("!!Warning!!");
+                Console.WriteLine("Password must be at least 8 characters long containing only letters, numbers and special characters");
+                Console.WriteLine("Enter Password: ");
+                do
+                {
+                    info = Console.ReadKey(true);
+                    if (info?.Key == ConsoleKey.Enter)
+                    {
+                        break;
+                    }
+                    else if (info?.Key == ConsoleKey.Backspace)
+                    {
+                        int pos = Console.CursorLeft;
+                        if (password.Length > 0)
+                        {
+                            password.Length--;
+                            Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                            Console.Write(" ");
+                            Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                            Console.SetCursorPosition(pos, Console.CursorTop);
+                        }
+                    }
+                    else
+                    {
+                        password.Append(info?.KeyChar);
+                        Console.Write("*");
+                    }
+                } while (true);
+
+                if (!PasswordRegex.IsMatch(password.ToString()))
+                {
+                    Console.WriteLine("\nInvalid password try again ... \n\n");
+                    password.Clear();
+                }
+                else
+                {
+                    break;
+                }
+            } while (true);
+
+            return password.ToString();
+        }
+
+
+        delegate void Mydelegate(string message);
+    }
+}
--- IT step/Program.cs
+++ IT step/Program.cs
@@ -1,49 +1,109 @@
-﻿
-PartyOrginiser orginiser = new();
-Guest guest = new();
-guest.Subscribe(orginiser);
-orginiser.CutCake(DateTime.Now);
+﻿using System.Text;
+using System.Text.RegularExpressions;
 
-public class MyEventArgs : EventArgs
+//task 1
+Console.WriteLine("Task1 : \n\n");
+Login();
+
+Console.WriteLine("Task 2 and task 3 : \n\n");
+//task 2 & task 3
+Mydelegate AnonimousDel = delegate (string message)
 {
-    public string Message { get; }
-    public MyEventArgs(string message)
+    Console.WriteLine(message);
+};
+Mydelegate lambdaDel= message=>Console.WriteLine(message);
+
+AnonimousDel("Invoked by Anonimous");
+lambdaDel("Invoked by lambda");
+
+
+static void Login()
+{
+    string password = passwordValidation();
+    Console.Write("\n\n");
+
+    //mail validation
+    string email;       //at least one lowercase, uppercase, number, special. emil
+    Regex EmailRegex = new(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&]{8,}@([\w-]{2,}\.)+[\w-]{2,4}$");
+    do
     {
-        Message = message;
-    }
+        Console.WriteLine("!! warning !!");
+
+        Console.WriteLine("Email should contain at least one lower one Upper one number and one special character \n" +
+        "should be at least 12 characters long");
+
+        Console.WriteLine("Enter Email: ");
+        
+        email = Console.ReadLine()!;
+        if (!EmailRegex.IsMatch(email))
+        {
+            Console.WriteLine("\nInvalid Email try again ... \n\n");
+        }
+        else
+        {
+            break;
+        }
+    }while (true);
+
+    Console.WriteLine($"\npassword: {password} and email:{email} are valid");
 }
 
-public class PartyOrginiser
+
+//password generation & validation
+static string passwordValidation()
 {
-    public event EventHandler<MyEventArgs>? CakeCutEventEvent;
+    StringBuilder password = new();
+    ConsoleKeyInfo? info;
+    Regex PasswordRegex = new(@"(^[a-zA-Z0-9.!_%+-/]).{7,}$");
 
-    protected virtual void OnCakeCutEvent(string message)
+    do
     {
-        MyEventArgs args = new MyEventArgs(message);
-        CakeCutEventEvent?.Invoke(this, args);
-    }
+        Console.WriteLine("!!Warning!!");
+        Console.WriteLine("Password must be at least 8 characters long containing only letters, numbers and special characters");
+        Console.WriteLine("Enter Password: ");
+        do
+        {
+            info = Console.ReadKey(true);
+            if (info?.Key == ConsoleKey.Enter)
+            {
+                break;
+            }
+            else if (info?.Key == ConsoleKey.Backspace)
+            {
+                int pos = Console.CursorLeft;
+                if (password.Length > 0)
+                {
+                    password.Length--;
+                    Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                    Console.Write(" ");
+                    Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                }
+                else
+                {
+                    Console.Write(" ");
+                    Console.SetCursorPosition(pos, Console.CursorTop);
+                }
+            }
+            else
+            {
+                password.Append(info?.KeyChar);
+                Console.Write("*");
+            }
+        } while (true);
 
-    public void CutCake(DateTime date)
-    {
-        OnCakeCutEvent($"Cake cutting starts at: {date}");
-    }
+        if (!PasswordRegex.IsMatch(password.ToString()))
+        {
+            Console.WriteLine("\nInvalid password try again ... \n\n");
+            password.Clear();
+        }
+        else
+        {
+            break;
+        }
+    } while (true);
+
+    return password.ToString();
 }
 
-//Subscriber
-public class Guest
-{
-    private void VideoUploadEventHandler(object sender, MyEventArgs e)
-    {
-        Console.WriteLine($"Alert! new notification from Party organisers:\n{e.Message}");
-    }
 
-    public void Subscribe(PartyOrginiser orginiser)
-    {
-        orginiser.CakeCutEventEvent += VideoUploadEventHandler;
-    }
-
-    public void UnSubscribe(PartyOrginiser orginiser)
-    {
-        orginiser.CakeCutEventEvent -= VideoUploadEventHandler;
-    }
-}
+delegate void Mydelegate(string message);
