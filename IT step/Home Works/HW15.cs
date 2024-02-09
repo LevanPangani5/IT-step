15621c97358acbde036d42c46ae25d2148a4b7d6
LevanPangani5 <levanfangani55@gmail.com>
2024-02-09 20:18:26 +0400

    adds HW15

---
+++ IT step/Home Works/HW15.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT_step.Home_Works
+{
+    internal class HW15
+    {
+
+        static void GenPalindromes()
+        {
+            int[] arr = new int[1000000];
+            Random rand = new();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(10000, 99999);
+            }
+
+            var palindrome = arr.Where(x => IsPalindrome(x)).ToArray();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(palindrome[i]);
+            }
+        }
+
+
+        static bool IsPalindrome(int numb)
+        {
+            string data = numb.ToString();
+            int startI = 0;
+            int endI = data.Length - 1;
+            while (startI < data.Length / 2)
+            {
+                if (data[startI] != data[endI])
+                {
+                    return false;
+                }
+                startI++;
+                endI--;
+            }
+            return true;
+        }
+
+    }
+}
--- IT step/Program.cs
+++ IT step/Program.cs
@@ -1,109 +1,31 @@
-﻿using System.Text;
-using System.Text.RegularExpressions;
+﻿int[] arr = new int[1000000];
+Random rand = new();
 
-//task 1
-Console.WriteLine("Task1 : \n\n");
-Login();
-
-Console.WriteLine("Task 2 and task 3 : \n\n");
-//task 2 & task 3
-Mydelegate AnonimousDel = delegate (string message)
+for(int i =0; i < arr.Length; i++)
 {
-    Console.WriteLine(message);
-};
-Mydelegate lambdaDel= message=>Console.WriteLine(message);
-
-AnonimousDel("Invoked by Anonimous");
-lambdaDel("Invoked by lambda");
-
-
-static void Login()
-{
-    string password = passwordValidation();
-    Console.Write("\n\n");
-
-    //mail validation
-    string email;       //at least one lowercase, uppercase, number, special. emil
-    Regex EmailRegex = new(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&]{8,}@([\w-]{2,}\.)+[\w-]{2,4}$");
-    do
-    {
-        Console.WriteLine("!! warning !!");
-
-        Console.WriteLine("Email should contain at least one lower one Upper one number and one special character \n" +
-        "should be at least 12 characters long");
-
-        Console.WriteLine("Enter Email: ");
-        
-        email = Console.ReadLine()!;
-        if (!EmailRegex.IsMatch(email))
-        {
-            Console.WriteLine("\nInvalid Email try again ... \n\n");
-        }
-        else
-        {
-            break;
-        }
-    }while (true);
-
-    Console.WriteLine($"\npassword: {password} and email:{email} are valid");
+    arr[i] = rand.Next(10000,99999);
 }
 
+var palindrome = arr.Where(x => isPalindrome(x)).ToArray();
 
-//password generation & validation
-static string passwordValidation()
+for(int i=0; i < 10; i++)
 {
-    StringBuilder password = new();
-    ConsoleKeyInfo? info;
-    Regex PasswordRegex = new(@"(^[a-zA-Z0-9.!_%+-/]).{7,}$");
-
-    do
-    {
-        Console.WriteLine("!!Warning!!");
-        Console.WriteLine("Password must be at least 8 characters long containing only letters, numbers and special characters");
-        Console.WriteLine("Enter Password: ");
-        do
-        {
-            info = Console.ReadKey(true);
-            if (info?.Key == ConsoleKey.Enter)
-            {
-                break;
-            }
-            else if (info?.Key == ConsoleKey.Backspace)
-            {
-                int pos = Console.CursorLeft;
-                if (password.Length > 0)
-                {
-                    password.Length--;
-                    Console.SetCursorPosition(pos - 1, Console.CursorTop);
-                    Console.Write(" ");
-                    Console.SetCursorPosition(pos - 1, Console.CursorTop);
-                }
-                else
-                {
-                    Console.Write(" ");
-                    Console.SetCursorPosition(pos, Console.CursorTop);
-                }
-            }
-            else
-            {
-                password.Append(info?.KeyChar);
-                Console.Write("*");
-            }
-        } while (true);
-
-        if (!PasswordRegex.IsMatch(password.ToString()))
-        {
-            Console.WriteLine("\nInvalid password try again ... \n\n");
-            password.Clear();
-        }
-        else
-        {
-            break;
-        }
-    } while (true);
-
-    return password.ToString();
+    Console.WriteLine(palindrome[i]);
 }
 
-
-delegate void Mydelegate(string message);
+ static bool isPalindrome(int numb)
+{
+    string data = numb.ToString();
+    int startI = 0;
+    int endI = data.Length - 1;
+    while (startI < data.Length / 2)
+    {
+        if(data[startI] != data[endI])
+        {
+            return false;
+        }
+        startI++;
+        endI--;
+    }
+    return true;
+}
